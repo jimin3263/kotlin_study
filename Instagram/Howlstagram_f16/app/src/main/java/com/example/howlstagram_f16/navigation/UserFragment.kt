314206de377a774f5012aa7266ecfc57ac080200
jimin3263 <dkdldsla0000@naver.com>
2021-05-24 00:28:56 +0900

    feat: following & follower

--- Instagram/Howlstagram_f16/app/src/main/java/com/example/howlstagram_f16/navigation/UserFragment.kt
+++ Instagram/Howlstagram_f16/app/src/main/java/com/example/howlstagram_f16/navigation/UserFragment.kt
@@ -1,6 +1,7 @@
 package com.example.howlstagram_f16.navigation
 
 import android.content.Intent
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,6 +20,7 @@ import com.example.howlstagram_f16.LoginActivity
 import com.example.howlstagram_f16.MainActivity
 import com.example.howlstagram_f16.R
 import com.example.howlstagram_f16.navigation.model.ContentDTO
+import com.example.howlstagram_f16.navigation.model.FollowDTO
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
@@ -66,6 +69,10 @@ class UserFragment: Fragment() {
             mainactivity?.toolbar_title_image?.visibility = View.GONE
             mainactivity?.toolbar_username?.visibility = View.VISIBLE
             mainactivity?.toolbar_btn_back?.visibility = View.VISIBLE
+
+            fragmentView?.account_btn_follow_signout?.setOnClickListener {
+                requestFollow()
+            }
         }
 
         fragmentView?.account_recyclerview?.adapter = UserFragmentRecyclerViewAdapter()
@@ -78,7 +85,85 @@ class UserFragment: Fragment() {
             activity?.startActivityForResult(photoPickerIntent,PICK_PROFILE_FROM_ALBUM)
         }
         getProfileImage(fragmentView)
+        getFollowerAndFollowing(fragmentView)
         return fragmentView
+    }
+
+    //데이터베이스에서 팔로워 팔로잉 수 가져옴
+    fun getFollowerAndFollowing(fragmentView:View){
+        firestore?.collection("users")?.document(uid!!)?.addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
+            if (documentSnapshot == null)
+                return@addSnapshotListener
+            var followDTO = documentSnapshot.toObject(FollowDTO::class.java)
+            if (followDTO?.followerCount != null){
+                fragmentView?.account_tv_follower_count?.text = followDTO?.followerCount?.toString()
+                if (followDTO?.followers.containsKey(currentUserUid!!)){
+                    fragmentView?.account_btn_follow_signout?.text = getString(R.string.follow_cancel)
+                    fragmentView?.account_btn_follow_signout?.background?.setColorFilter(ContextCompat.getColor(activity!!,R.color.colorLightGray),PorterDuff.Mode.MULTIPLY)
+
+                }else{
+                    if(uid!=currentUserUid){
+                        fragmentView?.account_btn_follow_signout?.text = getString(R.string.follow)
+                        fragmentView?.account_btn_follow_signout?.background?.colorFilter = null
+                    }
+                }
+            }
+            if (followDTO?.followingCount != null){
+                fragmentView?.account_tv_following_count?.text = followDTO?.followingCount?.toString()
+            }
+        }
+    }
+
+    //팔로워, 팔로잉
+    fun requestFollow(){
+        var tsDocFollowing = firestore?.collection("users")?.document(currentUserUid!!)
+        firestore?.runTransaction { transaction ->
+            var followDTO = transaction.get(tsDocFollowing!!).toObject(FollowDTO::class.java)
+            //내 계정 -> following
+            if (followDTO == null){
+                followDTO = FollowDTO()
+                followDTO!!.followingCount =1
+                followDTO!!.followings[uid!!]=true
+
+                transaction.set(tsDocFollowing,followDTO)
+                return@runTransaction
+            }
+            if (followDTO.followings.containsKey(uid)){
+                //팔로우 되어 있음 -> 취소
+                followDTO?.followingCount=followDTO?.followingCount -1
+                followDTO?.followings?.remove(uid)
+            }else{
+                //팔로우 되어 있지 않음 -> 팔로잉
+                followDTO?.followingCount = followDTO?.followingCount +1
+                followDTO?.followings[uid!!] = true
+            }
+            transaction.set(tsDocFollowing,followDTO)
+            return@runTransaction
+        }
+        //follower
+        var tsDocFollower = firestore?.collection("users")?.document(uid!!)
+        firestore?.runTransaction { transaction ->
+            var followDTO= transaction.get(tsDocFollower!!).toObject(FollowDTO::class.java)
+
+            if(followDTO == null){
+                followDTO = FollowDTO()
+                followDTO!!.followerCount=1
+                followDTO!!.followers[currentUserUid!!] = true
+
+                transaction.set(tsDocFollower,followDTO!!)
+                return@runTransaction
+            }
+            if (followDTO!!.followers.containsKey(currentUserUid)){
+                followDTO!!.followerCount = followDTO!!.followerCount -1
+                followDTO!!.followers.remove(currentUserUid!!)
+            }else{
+                followDTO!!.followerCount = followDTO!!.followerCount +1
+                followDTO!!.followers[currentUserUid!!]= true
+            }
+            transaction.set(tsDocFollower,followDTO!!)
+            return@runTransaction
+        }
+
     }
 
     //프로필 사진 가져오기
